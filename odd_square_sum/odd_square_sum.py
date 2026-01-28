ca4653c6c9c3045977adcbc2dfe14ef46be9cf75
BrandoKlawrissian <54420282+BrandoKlawrissian@users.noreply.github.com>
2026-01-28 11:04:40 -0600

    Implement sum of odd squares in odd_square_sum.py
    Calculate the sum of odd square numbers up to 980,000.

---
+++ odd_square_sum/odd_square_sum.py
@@ -0,0 +1,11 @@
+#A number is a perfect square, or a square number, if it is the square of a positive integer. 
+#For example, is a square number because ; it is also an odd square. 
+#The first 5 square numbers are: , and the sum of the odd squares is . 
+#Among the first 980 thousand square numbers, what is the sum of all the odd squares? 
+#Show your work.
+
+m = 490_000
+
+result = m * (2*m - 1) * (2*m + 1) // 3
+
+print(result)
