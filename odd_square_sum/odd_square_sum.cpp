d373d5ea3c86bc5b66884f88edbd7ad2bf092630
BrandoKlawrissian <54420282+BrandoKlawrissian@users.noreply.github.com>
2026-01-28 11:10:59 -0600

    Add comment block about perfect and odd squares
    Added a comment block explaining perfect squares and odd squares.

--- odd_square_sum/odd_square_sum.cpp
+++ odd_square_sum/odd_square_sum.cpp
@@ -1,3 +1,9 @@
+/*A number is a perfect square, or a square number, if it is the square of a positive integer. 
+#For example, is a square number because ; it is also an odd square. 
+#The first 5 square numbers are: , and the sum of the odd squares is . 
+#Among the first 980 thousand square numbers, what is the sum of all the odd squares? 
+#Show your work.*/
+
 #include <iostream>
 using namespace std;
 
